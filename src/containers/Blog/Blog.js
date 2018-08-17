4d42fde013c120a6a9e5504041bb088e7b6a62c1
Nghia <kakabbage@gmail.com>
2018-08-17 13:23:09 +0700

    159 http-ajax wrapped up

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,28 +1,65 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
-import Post from '../../components/Post/Post';
-import FullPost from '../../components/FullPost/FullPost';
-import NewPost from '../../components/NewPost/NewPost';
+// import axios from 'axios';
+import axios from '../../axios';
+import Post from '../../components/Posts/Post';
+import FullPost from '../../components/Posts/FullPost';
+import NewPost from '../../components/Posts/NewPost';
 import './Blog.css';
 
+// const POSTS_PATH = 'https://jsonplaceholder.typicode.com/posts/';
+
 class Blog extends Component {
-    render () {
-        return (
-            <div>
-                <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
-                </section>
-                <section>
-                    <FullPost />
-                </section>
-                <section>
-                    <NewPost />
-                </section>
-            </div>
-        );
+  state = {
+    posts         : [],
+    selectedPostId: null,
+    error         : false,
+  };
+  
+  componentDidMount () {
+    axios.get('posts/')
+         .then(response => {
+           const posts = response.data.slice(0, 4);
+           const updatedPosts = posts.map(post => {
+             return {...post, author: "Max"};
+           });
+           this.setState({posts: updatedPosts});
+           //console.log(response.data);
+         })
+         .catch(error => {this.setState({error: true});})
+    ;
+  }
+  
+  selectPost = (id) => {
+    this.setState({selectedPostId: id});
+  };
+  
+  render () {
+    let posts = <p style={{textAlign: 'center'}}>Error!!!</p>;
+    if (!this.state.error) {
+      posts = this.state.posts.map(post => {
+        return <Post key={post.id}
+                     title={post.title}
+                     author={post.author}
+                     clicked={() => this.selectPost(post.id)} />;
+      });
     }
+    return (
+      
+      <div>
+        <section className="Posts">
+          {posts}
+        </section>
+        <section>
+          <FullPost id={this.state.selectedPostId} />
+        </section>
+        <section>
+          <NewPost />
+        </section>
+      </div>
+    )
+      ;
+  }
 }
 
 export default Blog;
